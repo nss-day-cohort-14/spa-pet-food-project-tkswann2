8d02ab5652aba19df8e72d76a8487d8e0cffe2da
Thomas Swann <tkswann@mac.com>
2016-06-07 21:20:24 -0500

    formatted cat json data

--- food.js
+++ food.js
@@ -12,7 +12,7 @@ var catFoodJSON = new XMLHttpRequest();
 catFoodJSON.addEventListener('load', function () {
   outputDiv = document.getElementById('output');
 });
-catFoodJSON.addEventListener('load', parseAfterLoaded);
+catFoodJSON.addEventListener('load', parseCatFoodJSON);
 catFoodJSON.addEventListener('error', jsonError);
 
 catFoodJSON.open('GET', 'catfood.json');
@@ -32,30 +32,27 @@ function jsonError () {
 function parseDogFoodJSON (progressEvent) {
 	var outputDiv = document.getElementById('output');
 	let parsedData = JSON.parse(progressEvent.target.responseText);
-	console.log(parsedData);
 	let newHTMLString = '';
 
 	for (var index = 0, l = parsedData.dog_brands.length; index < l; index++) {
 		var	value = parsedData.dog_brands[index];
 		let foodBrand = value.name;
-			console.log(foodBrand);
 			newHTMLString += `<petcard class="petcard">
-																				<header>
-																					<h3 id="foodBrand">${foodBrand}</h3>
-																				</header>
-																				<productimg id="imgBox">
-																					<img src="http://www.azic.com/wp-content/uploads/2013/09/dog-food.jpg" />
-																				</productimg>
+													<header>
+														<h3 id="foodBrand">${foodBrand}</h3>
+													</header>
+													<productimg id="imgBox">
+														<img src="http://www.azic.com/wp-content/uploads/2013/09/dog-food.jpg" />
+													</productimg>
 
-																				<info id="infoBox">
-																				<header>
-																					<h5>Types:</h5>
-																				</header>`;
+													<info id="infoBox">
+													<header>
+														<h5>Types:</h5>
+													</header>`;
 
 			for (var j = 0, x = value.types.length; j < x; j++) {
 				var foodTypeObj = value.types[j];
 				let foodType = value.types[j].type;
-				console.log(foodType);
 				newHTMLString += `	<div class="priceBox">
 																				<h6>${foodType}</h6>`;
 
@@ -63,8 +60,6 @@ function parseDogFoodJSON (progressEvent) {
 					var foodPriceObj = foodTypeObj.volumes[y];
 					let foodSize = foodPriceObj.name;
 					let foodPrice = foodPriceObj.price;
-					console.log(foodSize);
-					console.log(foodPrice);
 					newHTMLString += `        <ul>
 																						<li class="productSize">${foodSize}</li>
 																						<li class="productPrice">${foodPrice}</li>
@@ -82,6 +77,103 @@ function parseDogFoodJSON (progressEvent) {
 
 
 
+function parseCatFoodJSON () {
+	var outputDiv = document.getElementById('output');
+	var parsedData = JSON.parse(this.responseText);
+	var catData = parsedData.cat_brands;
+	var htmlString = '';
+	// console.log(parsedData.cat_brands);
+
+	for (var i = 0, j = catData.length; i < j; i++) {
+		var catBrands = catData[i];
+		var catBrandLabel = catBrands.brand;
+			console.log(catBrandLabel);
+			htmlString += `<div class="catFood">
+											<h2>${catBrandLabel}</h2>`;
+
+		for (var x = 0, y = catBrands.breeds.length; x < y; x++) {
+			var catBreeds = catBrands.breeds[x];
+			var catBreedLabel = catBreeds.breed;
+			var catVolumes = catBreeds.volumes;
+				console.log(catBreedLabel);
+				htmlString += `<div id="catData">
+												<h5>${catBreedLabel}</h5>`;
+			for (var a = 0, b = catVolumes.length; a < b; a++) {
+				var catFoodData = catVolumes[a];
+				var catName = catFoodData.name;
+				var catPrice = catFoodData.price;
+				htmlString += `<p>${catName} :: ${catPrice}</p>`;
+			}
+			htmlString += `</div>`;
+		}
+		htmlString += `</div>`;
+	  outputDiv.innerHTML += htmlString;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**********************
 
@@ -91,29 +183,30 @@ solution from Dan
 
 
 
-function parseAfterLoaded() {
-  outputDiv; //loads selectedDiv into memory before it changes value
-  var data = JSON.parse(event.target.responseText);   //get and parse JSON data file
-  var newHTML = "";  //newHTML will be the final result added to the DOM
 
-  //the following loops through every object in the JSON file, breaking each one down until it gets to its end value, then adds those key-value pairs to newHTML 
- for (var item in data) {
-    newHTML += `<h3>${item}:</h3>`
-  function breakDownJSON (y) {
-    for (var x in y) {
-      var newVar = y[x];
-      if (newVar.constructor !== Array && newVar.constructor !== Object) {
-        newHTML += `<p>${x}: ${newVar}</p>`;
-      } else {
-          breakDownJSON(newVar);
-        }
-    }
-  } 
-    breakDownJSON(data[item]);
-  }
-  //we define selectedDiv below
-  outputDiv.innerHTML += newHTML;
-}
+// function parseAfterLoaded() {
+//   outputDiv; //loads selectedDiv into memory before it changes value
+//   var data = JSON.parse(event.target.responseText);   //get and parse JSON data file
+//   var newHTML = "";  //newHTML will be the final result added to the DOM
+
+//   //the following loops through every object in the JSON file, breaking each one down until it gets to its end value, then adds those key-value pairs to newHTML 
+//  for (var item in data) {
+//     newHTML += `<h3>${item}:</h3>`
+//   function breakDownJSON (y) {
+//     for (var x in y) {
+//       var newVar = y[x];
+//       if (newVar.constructor !== Array && newVar.constructor !== Object) {
+//         newHTML += `<p>${x}: ${newVar}</p>`;
+//       } else {
+//           breakDownJSON(newVar);
+//         }
+//     }
+//   } 
+//     breakDownJSON(data[item]);
+//   }
+//   //we define selectedDiv below
+//   outputDiv.innerHTML += newHTML;
+// }
 
 
 
